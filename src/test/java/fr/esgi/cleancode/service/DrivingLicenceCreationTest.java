b1c2c6fc0e36e4947b23b0b4fb9f645efc75809f
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2022-11-30 13:06:46 +0100

    Red step for the driving licence should be created with twelve points

---
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceCreationTest.java
@@ -0,0 +1,40 @@
+package fr.esgi.cleancode.service;
+
+import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.model.DrivingLicence;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class DrivingLicenceCreationTest {
+
+    @Mock
+    InMemoryDatabase inMemoryDatabase;
+
+    @InjectMocks
+    DrivingLicenceGenerationService drivingLicenceGenerationService;
+
+    @InjectMocks
+    DrivingLicenceIdGenerationService drivingLicenceIdGenerationService;
+
+    @Captor
+    ArgumentCaptor<DrivingLicence> availablePoints;
+
+
+    @Test
+    void drivingLicenceShouldHaveTwelvePoints() {
+        verify(drivingLicenceGenerationService).generate(availablePoints.capture());
+        final var drivingLicensePoint = availablePoints.getValue();
+        // THEN
+        assertThat(drivingLicensePoint.getAvailablePoints()).isEqualTo(12);
+    }
+
+}
